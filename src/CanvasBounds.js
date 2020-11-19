4fe66e2e6bff5e5a42c9360de3c22e469702704a
drivej@hotmail.com <drivej@hotmail.com>
2020-11-19 18:09:39 -0500

    ts

--- src/CanvasBounds.js
+++ src/CanvasBounds.js
@@ -1,24 +1,28 @@
 import { clamp } from './CanvasUtils';
 
-export function CanvasBounds(config) {
-  var self = this;
-  this.minX = -Infinity;
-  this.maxX = Infinity;
-  this.minY = -Infinity;
-  this.maxY = Infinity;
-  this.minZ = -Infinity;
-  this.maxZ = Infinity;
+export class CanvasBounds {
+  constructor(config) {
+    var self = this;
+    this.minX = -Infinity;
+    this.maxX = Infinity;
+    this.minY = -Infinity;
+    this.maxY = Infinity;
+    this.minZ = -Infinity;
+    this.maxZ = Infinity;
 
-  this.set = function (o) {
+    this.set(config);
+  }
+
+  set(o) {
     for (var e in o) {
       this[e] = o[e];
     }
-  };
-  this.clamp = function (p) {
+  }
+
+  clamp(p) {
     if ('x' in p) p.x = clamp(p.x, self.minX, self.maxX);
     if ('y' in p) p.y = clamp(p.y, self.minY, self.maxY);
     if ('z' in p) p.z = clamp(p.z, self.minZ, self.maxZ);
     return p;
-  };
-  this.set(config);
+  }
 }
